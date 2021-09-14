8b6f48f429ea88f15eb40d7ed94de4e5e8726d0d
Haykal <76930536+HorlandoSR@users.noreply.github.com>
2021-09-14 11:05:43 +0700

    Update Loader.php

--- src/ElytraFly/Loader.php
+++ src/ElytraFly/Loader.php
@@ -13,9 +13,9 @@ class Loader extends PluginBase implements Listener
 {
 	
 	public function onEnable(){
-		$this->getLogger()->info("ElytraFly Enabled");
+		$this->getLogger()->info("ArmorFLY Enabled");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		if(!$this->getDescription()->getAuthors() === "KRUNCH7SHooT"){
+		if(!$this->getDescription()->getAuthors() === "KRUNCH7SHooT And HaykalPRO"){
 			$this->getPluginLoader()->disablePlugin($this);
 		}
 	}
@@ -23,7 +23,7 @@ class Loader extends PluginBase implements Listener
 	public function onArmor(EntityArmorChangeEvent $ev){
 		$sender = $ev->getEntity();
 		$item = $ev->getNewItem();
-		if($item->getId() == 444){
+		if($item->getId() == 310){
 			$sender->setAllowFlight(true);
 			$sender->setFlying(true);
 			$volume = mt_rand();
@@ -32,7 +32,58 @@ class Loader extends PluginBase implements Listener
 			$sender->setAllowFlight(false);
 			$sender->setFlying(false);
 		}
-		if(!$item->getId() == 444){
+		if(!$item->getId() == 310){
+			$sender->setAllowFlight(false);
+			$sender->setFlying(false);
+		}
+	}
+	
+	$sender = $ev->getEntity();
+		$item = $ev->getNewItem();
+		if($item->getId() == 311){
+			$sender->setAllowFlight(true);
+			$sender->setFlying(true);
+			$volume = mt_rand();
+			$sender->getLevel()->broadcastLevelSoundEvent($sender, LevelSoundEventPacket::SOUND_LEVELUP, (int) $volume);
+		} else {
+			$sender->setAllowFlight(false);
+			$sender->setFlying(false);
+		}
+		if(!$item->getId() == 311){
+			$sender->setAllowFlight(false);
+			$sender->setFlying(false);
+		}
+	}
+        
+        $sender = $ev->getEntity();
+		$item = $ev->getNewItem();
+		if($item->getId() == 312){
+			$sender->setAllowFlight(true);
+			$sender->setFlying(true);
+			$volume = mt_rand();
+			$sender->getLevel()->broadcastLevelSoundEvent($sender, LevelSoundEventPacket::SOUND_LEVELUP, (int) $volume);
+		} else {
+			$sender->setAllowFlight(false);
+			$sender->setFlying(false);
+		}
+		if(!$item->getId() == 312){
+			$sender->setAllowFlight(false);
+			$sender->setFlying(false);
+		}
+	}
+
+        $sender = $ev->getEntity();
+		$item = $ev->getNewItem();
+		if($item->getId() == 313){
+			$sender->setAllowFlight(true);
+			$sender->setFlying(true);
+			$volume = mt_rand();
+			$sender->getLevel()->broadcastLevelSoundEvent($sender, LevelSoundEventPacket::SOUND_LEVELUP, (int) $volume);
+		} else {
+			$sender->setAllowFlight(false);
+			$sender->setFlying(false);
+		}
+		if(!$item->getId() == 313){
 			$sender->setAllowFlight(false);
 			$sender->setFlying(false);
 		}
